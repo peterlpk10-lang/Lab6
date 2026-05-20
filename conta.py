85c72e6b9531378129ad25d0ecbd098d8532be2d
peterlpk10-lang <peterlpk10@gmail.com>
2026-05-20 17:00:16 -0300

    correção
    correção de uns trem

--- conta.py
+++ conta.py
@@ -1,16 +1,17 @@
 class Conta:
-    def __init__(self):
+    def __init__(self, nome):
+        self.__nome = nome
         self.__saldo = 0
     
     def sacar(self, valor_saque):
         self.__saldo -= valor_saque
 
     def depositar(self, valor_deposito):
-         self.__saldo += valor_saque
+         self.__saldo += valor_deposito
 
     def calcular_rendimento(self):
         return self.__saldo * 0.1
 
     def mostrar_saldo(self):
-        print(f"Saldo: {self.__saldo}")
+        print(f"SALDO ATUAL: R${self.__saldo}")
 
