5837eeffbf028e22c4ab7ec66c1d30f910ff5c81
peterlpk10-lang <peterlpk10@gmail.com>
2026-05-20 16:25:42 -0300

    Update conta.py
    correção do nome dos métodos.

--- conta.py
+++ conta.py
@@ -2,15 +2,15 @@ class Conta:
     def __init__(self):
         self.__saldo = 0
     
-    def saca(self, valor_saque):
+    def sacar(self, valor_saque):
         self.__saldo -= valor_saque
 
-    def deposita(self, valor_deposito):
+    def depositar(self, valor_deposito):
          self.__saldo += valor_saque
 
-    def calcula_rendimento(self):
+    def calcular_rendimento(self):
         return self.__saldo * 0.1
 
-    def mostra_saldo(self):
+    def mostrar_saldo(self):
         print(f"Saldo: {self.__saldo}")
 
